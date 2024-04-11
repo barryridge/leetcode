5996534295ffb81ab7edffd62dd2920ba70b06a9
Barry Ridge <bridge@jpl.nasa.gov>
2024-04-10 23:02:03 -0700

    Add Two Sum solution

---
+++ two-sum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and (_hash:=hashmap[complement]) != i:
+                return [i, _hash]
